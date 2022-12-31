42914d0a250df598d6f414bc74307419e2ea6c97
iswilljr <iswilljr@gmail.com>
2022-12-31 18:43:32 -0500

    chore(client): initialize client

---
+++ client/next.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+
+await import("./env/server.mjs");
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
+export default config;

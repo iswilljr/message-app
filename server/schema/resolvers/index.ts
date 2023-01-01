f5278248744cca178b5ed37aee2e4a856208d63b
iswilljr <iswilljr@gmail.com>
2023-01-01 18:52:37 -0500

    feat(server): add conversations query to schema resolvers

--- server/schema/resolvers/index.ts
+++ server/schema/resolvers/index.ts
@@ -1,10 +1,12 @@
 import { createConversation } from "./mutation/create-conversation.js";
 import { createUsername } from "./mutation/create-username.js";
+import { conversations } from "./query/conversations.js";
 import { searchUsers } from "./query/search-users.js";
 
 export const resolvers: Resolvers = {
   Query: {
     searchUsers,
+    conversations,
   },
   Mutation: {
     createUsername,
---
+++ server/schema/resolvers/query/conversations.ts
@@ -0,0 +1,32 @@
+import { Prisma } from "@prisma/client";
+import { GraphQLError } from "graphql";
+
+export const conversations: QueryResolvers["conversations"] = async (_, _args, { prisma, session }) => {
+  if (!session) throw new GraphQLError("Unauthorized");
+
+  try {
+    const conversations = await prisma.conversation.findMany({
+      where: { participants: { some: { userId: { equals: session.user?.id } } } },
+      include: {
+        participants: populateParticipants,
+        latestMessage: populateLastMessages,
+      },
+    });
+
+    return conversations;
+  } catch (error: any) {
+    throw new GraphQLError(error.message);
+  }
+};
+
+const populateUser = Prisma.validator<Prisma.UserArgs>()({
+  select: { id: true, email: true, image: true, username: true },
+});
+
+const populateParticipants = Prisma.validator<Prisma.ConversationParticipantArgs>()({
+  select: { id: true, hasSeenLatestMessage: true, user: populateUser },
+});
+
+const populateLastMessages = Prisma.validator<Prisma.MessageArgs>()({
+  select: { id: true, sender: populateUser, node: true, createdAt: true },
+});
